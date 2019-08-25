64648918f541e9afab49caeb05e95e3233fecded
Vitor <vitor.thomazini94@gmail.com>
2019-08-25 18:01:23 -0300

    Validate text fields with onBlur and onChange Events

--- src/App.js
+++ src/App.js
@@ -1,53 +1,70 @@
-import React, {useEffect} from 'react';
-import logo from './logo.svg';
+import React, {useEffect,useState} from 'react';
 import './App.css';
 
 import useForm from './hoc/useForm';
 
+const INITIAL_FORM_DATA = {
+  name: {
+    value: '',
+    required: false,
+    changeAttr: {
+      maxLength: 5,
+    },
+  },
+  username: {
+    value: '',
+    required: true,
+    changeAttr: {
+      maxLength: 30,
+    },
+  },
+  password: {
+    value: '',
+    error: false,
+    valid: false,
+    required: true,
+    changeAttr: {
+      maxLength: 30,
+    },
+    blurAttr: {
+      minLength: 7,
+    },
+  },
+}
+
 function App() {
+  const [count, setCount] = useState(1);
 
   useEffect(() => {
     console.log('tetse')
   },[])
 
-  const validate = (values) => {
-    let errors = {};
-    if (values.username.value === '') {
-      errors.username = 'Username invalid!';
-    }
-    if (values.password.value === '') {
-      errors.password = 'Password invalid!';
-    }
-    return errors;
-  }
-
   const submitHandler = () => {
-    console.log(errors);
+    console.log('values', values)
   }
 
-  const { values, errors, onChange, onSubmit } = useForm(
+  const { values, onChange, onBlur ,onSubmit, setValues } = useForm (
+    INITIAL_FORM_DATA,
+    false,
     submitHandler,
-    {username: {
-      value: '',
-      valid: true,
-    }, 
-    password: {
-      value: '',
-      valid: true,
-    }},
-    validate,
-    validate,
   );
 
   return (
     <div className="App">
       <form onSubmit={onSubmit}>
+        <input type="text" name="name" value={values.name.value} placeholder="name" onChange={onChange}/>
         <input type="text" name="username" value={values.username.value} placeholder="username" onChange={onChange}/>
-        <input type="password" name="password" value={values.password.value} placeholder="password" onChange={onChange}/>
+        <input type="password" name="password" value={values.password.value} placeholder="password" onChange={onChange} onBlur={onBlur}/>
         <button type="submit">Salvar</button>
+        <button type="button" onClick={()=> {
+          const key = count + 'value';
+          setValues({
+            ...values,
+            [key]: 'NewValue' + count
+          });
+          setCount(count + 1);
+        }}>Click</button>
       </form>
-      {console.log('errors',errors)}
-      {console.log('values',values)}
     </div>
   );
 }
--- src/hoc/useForm.jsx
+++ src/hoc/useForm.jsx
@@ -1,38 +1,96 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
-export default (callback, initialState = {}, submitValidate, changeValidate) => {
+export default (initialState = {}, canResetFields, callback) => {
   const [values, setValues] = useState(initialState);
-  const [errors, setErrors] = useState({});
 
-  const onChange = (event) => { 
-    const newValues = {
-      ...values,
-      [event.target.name]:{
-        ...values[event.target.name],
-        value: event.target.value
-      }
+  const onChange = (event) => {
+    let currentValue = { ...values[event.target.name] }
+    currentValue.value = event.target.value;
+    if (currentValue.changeAttr) {
+      currentValue = validateFieldMaxLengthChangeFilter(currentValue);
     }
-    setErrors(changeValidate(newValues));
-    setValues(newValues);
+    setValues({
+      ...values,
+      [event.target.name]: { ...currentValue },
+    })
   };
+
+  const onBlur = (event) => {
+    let currentValue = { ...values[event.target.name] }
+    currentValue = validateFields(currentValue);
+    setValues({
+      ...values,
+      [event.target.name]: { ...currentValue },
+    })
+  }
+
+  const resetFields = () => {
+    if (canResetFields) {
+      setValues(initialState);
+    }
+  }
 
   const onSubmit = (event) => {
     event.preventDefault();
-    console.log(Object.keys(submitValidate(values)))
-    if (Object.keys(submitValidate(values)).length === 0) {
+    const fieldKeys = Object.keys(values);
+    const hasErrors = hasNotInvalidFields(fieldKeys, values);
+    console.log(hasErrors)
+    if (!hasErrors) {
       callback();
-      setValues(initialState);
-      setErrors({})
-    } else {
-      setErrors(submitValidate(values));
+      resetFields();
     }
   };
 
   return {
     onChange,
+    onBlur,
     onSubmit,
-    errors,
     values,
+    setValues,
   }
 
+}
+
+function hasNotInvalidFields(fieldKeys, values) {
+  return fieldKeys.some(fieldKey => {
+    const field = validateFields(values[fieldKey]);
+    return field.error ? true : false;
+  });
+}
+
+function validateFields(field) {
+  if (field.required) {
+    field = validateFieldRequiredFilter(field);
+  }
+  if (field.blurAttr) {
+    field = validateFieldMinLengthBlurFilter(field);
+  }
+  return field;
+}
+
+function validateFieldRequiredFilter(field) {
+  if (field.value.trim().length === 0) {
+    field.error = true;
+  } else {
+    field.error = false;
+  }
+  return field;
+}
+
+function validateFieldMinLengthBlurFilter(field) {
+  const ruleMinLength = field.blurAttr.minLength;
+  if (ruleMinLength && ruleMinLength > field.value.trim().length) {
+    field.error = true;
+  } else {
+    field.error = false;
+  }
+  return field;
+}
+
+function validateFieldMaxLengthChangeFilter(field) {
+  const ruleMaxLength = field.changeAttr.maxLength;
+  if (ruleMaxLength && ruleMaxLength <= field.value.trim().length) {
+    field.value = field.value.slice(0, ruleMaxLength);
+  }
+  return field;
 }
